4d23361c744996ab93bcb67cac68f4aec26bbbcf
Mohamed Mustafa <pmohmmedmustafa@gmail.com>
2023-11-04 19:42:18 +0200

    Signal Modification Completed
    x and y variables changed to x_phase, y_phase, x_apmlitude, y_amplitude

--- helper_functions.py
+++ helper_functions.py
@@ -11,7 +11,7 @@ def draw(x1=[], y1=[], x2=None, y2=None, label1="", label2="", type="disctete", 
     plt.close()
     plt.axhline(0, color='black')
     count = 0
-    if x1 is not None and y1 is not None and len(x1) > 0:
+    if (x1 is not None )and (y1 is not None )and (len(x1) > 0):
 
         count += 1
 
@@ -20,14 +20,14 @@ def draw(x1=[], y1=[], x2=None, y2=None, label1="", label2="", type="disctete", 
 
         if(type == "discrete" or type == 'both'):
             plt.stem(x1, y1, linefmt='-',
-                     markerfmt='ro', basefmt=' ')
+                     markerfmt='ro', basefmt=' ', label=label1)
         # if (type == "continuous" or type == 'both'):
         #     plt.plot(x1, y1, label=label1, color='black')
 
-    if x2 is not None and y2 is not None and len(x2) > 0:
+    if (x2 is not None )and (y2 is not None )and (len(x2) > 0):
         if(type == "discrete" or type == 'both'):
             plt.stem(x2, y2, linefmt='-',
-                     basefmt=' ')
+                     basefmt=' ', label=label2)
         if (type == "continuous" or type == 'both'):
             plt.plot(x2, y2,  label=label2, color='black')
 
@@ -38,7 +38,7 @@ def draw(x1=[], y1=[], x2=None, y2=None, label1="", label2="", type="disctete", 
     plt.title(title)
     plt.grid(True)
     if(count > 1):
-        plt.legend(loc='upper right')
+        plt.legend(loc='lower right')
 
     plt.show()
 
