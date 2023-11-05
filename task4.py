eb4d2be2cfa88731a24d4dff0127a2b84c78c7ff
MhmdMhros <mhrwsmhmd8882@gmail.com>
2023-11-05 18:46:24 +0200

    complete my task (task4)

--- task4.py
+++ task4.py
@@ -10,6 +10,7 @@ from tkinter.ttk import *
 windwo = dft_frame = dft_choice = option1_radio = option2_radio = option1_radio = option2_radio = file_frame = dft_label = dft_entry = dft_button = display_button = modify_frame = select_label = dropdown_var = fundamental_frequancies = inside_frame = A_label = Phase_label = A_entry = Phase_entry = modify_button = None
 x_phase= y_phase=x_amplitude= y_amblitude=None
 x = y = None
+Amplitude = Phase = Frequences = None
 def open_file(entry,dft_choice):
 
     file_path = filedialog.askopenfilename()
@@ -17,30 +18,32 @@ def open_file(entry,dft_choice):
         return
     file_name = os.path.basename(file_path)
 
-    global x, y
+    global Amplitude, Phase, Frequences, x, y
     if(dft_choice.get() == "DFT"):
         x, y = hf.read_signal_file(path=file_path)
         print("x = ", x)
         print("y = ", y)
     else:
-        x, y = hf.read_amplitude_phase_file(file_path=file_path)
-        print("(A) = ", x)
-        print("Phase = ", y)
+        Amplitude, Phase = hf.read_amplitude_phase_file(file_path=file_path)
+        print("(A) = ", Amplitude)
+        print("Phase = ", Phase)
     entry.delete(0, 'end')
     entry.insert(0, file_name)
 
-def display_wave(dft_choice):
-    global x, y
-    apply_dft_idft(dft_choice)
+def display_wave(dft_choice,Fs):
+    global Amplitude, Phase, Frequences, x, y
+    apply_dft_idft(dft_choice,Fs)
 
     if(dft_choice.get() == "DFT"):
         hf.isPeriodic = 1
-        hf.draw(x1=x, y1=y,
-                title="DFT Signal", type="both",label1="Phase", label2="Amplitude")
+        hf.draw(x1=Frequences, y1=Amplitude,
+                title="DFT Signal", type="discrete",label1="frequency", label2="amplitude")
+        hf.draw(x1=Frequences, y1=Phase,
+                title="DFT Signal", type="discrete", label1="frequency", label2="phase")
         # Format the numbers
-        x = [f'{i:.13f}f' for i in x]
-        y = [f'{i:.13f}f' for i in y]
-        hf.write_file('output.txt', signalType=hf.signalType, isPeriodic=hf.isPeriodic, N=hf.N, x=x, y=y)
+        Amplitude = [f'{i:.13f}f' for i in Amplitude]
+        Phase = [f'{i:.13f}f' for i in Phase]
+        hf.write_file('output.txt', signalType=hf.signalType, isPeriodic=hf.isPeriodic, N=hf.N, x=Amplitude, y=Phase)
     else:
         hf.isPeriodic = 0
         hf.draw(x1=x, y1=y,
@@ -53,24 +56,31 @@ def modified_wave():
     apply_modification()
     hf.draw(x1=x_phase, y1=y_phase,x2=x_amplitude, y2=y_amblitude,
             title="DFT Signal", type="discrete", label1="Phase", label2="Amplitude")
-def apply_dft_idft(dft_choice):
+def apply_dft_idft(dft_choice,Fs):
     # here to implement your function
-    global x, y
+    global Amplitude, Phase, Frequences, x, y
     if (dft_choice.get() == "DFT"):
-        x, y = dft(x_n=y, N=hf.N)
+        Frequences = calculate_frequences(sampling_frequency=float(Fs),N=hf.N)
+        Amplitude, Phase = dft(x_n=y, N=hf.N)
         # # Format the numbers
         # x = [f'{i:.13f}f' for i in x]
         # y = [f'{i:.13f}f' for i in y]
         # Print the results
-        print("(A) = ", x)
-        print("Phase = ", y)
+        print("Frequences = ", Frequences)
+        print("(A) = ", Amplitude)
+        print("Phase = ", Phase)
     else:
-        X_n = idft(amplitudes=x, phases=y, N=hf.N)
+        X_n = idft(amplitudes=Amplitude, phases=Phase, N=hf.N)
         x = np.array(range(hf.N))
         # Extract the real parts and store them in a list
         y = [round(xn.real) for xn in X_n]
         print("x = ", x)
         print("y = ", y)
+
+def calculate_frequences(sampling_frequency, N):
+    omega = (2 * np.pi) / (N * (1 / sampling_frequency)) # calculate fundamental frequency
+    frequencies = [omega * k for k in range(N)]
+    return frequencies
 def dft(x_n, N):
     x_k = np.zeros(N, dtype=np.complex128)
     for k in range(N):
@@ -121,8 +131,7 @@ def apply_modification():
 
 
 def open_gui(root):
-    global window, dft_frame , dft_choice , option1_radio , option2_radio , option1_radio , option2_radio , file_frame , dft_label , dft_entry , dft_button , display_button , modify_frame , select_label , dropdown_var , fundamental_frequancies , inside_frame , A_label , Phase_label , A_entry , Phase_entry , modify_button
-    
+    global window, dft_frame , dft_choice , option1_radio , option2_radio , option1_radio , option2_radio, frequancy_frame, frequancy_label, frequancy_entry, file_frame , dft_label , dft_entry , dft_button , display_button , modify_frame , select_label , dropdown_var , fundamental_frequancies , inside_frame , A_label , Phase_label , A_entry , Phase_entry , modify_button
     window =  tk.Toplevel(root)
     window.title("Frequency Domain Analysis")
     window.geometry("600x500")
@@ -150,6 +159,15 @@ def open_gui(root):
     option1_radio.grid(row=0,columnspan=5)
     option2_radio.grid(row=1,columnspan=5)
 
+    frequancy_frame = tk.Frame(dft_frame)
+    frequancy_label = tk.Label(frequancy_frame,
+                    text="Fs(kHz)",
+                    font=('Arial', 10))  # label
+    frequancy_entry = tk.Entry(frequancy_frame,width=50)  # entry
+    frequancy_label.grid(row=0,column=0)
+    frequancy_entry.grid(row=0,column=1)
+    frequancy_frame.grid(row=2,pady=7)
+
     file_frame = tk.Frame(dft_frame)
     dft_label = tk.Label(file_frame,
                     text="File:",
@@ -162,14 +180,14 @@ def open_gui(root):
     dft_label.grid(row=0, column=0)
     dft_entry.grid(row=0, column=1)
     dft_button.grid(row=0, column=2)
-    file_frame.grid(row=2,pady=20)
+    file_frame.grid(row=3,pady=20)
 
 
 
     display_button = tk.Button(dft_frame, text="Display",
-                            command=lambda :display_wave(dft_choice), width=50)  # button
+                            command=lambda :display_wave(dft_choice,frequancy_entry.get()), width=50)  # button
     # display
-    display_button.grid(row=3)
+    display_button.grid(row=4)
     # dft_frame.grid(padx=50, pady=15)
     dft_frame.pack()
     
