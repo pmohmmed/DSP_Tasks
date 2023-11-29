ff294753bd573b50da6e71129e1aeb6c19348407
MhmdMhros <mhrwsmhmd8882@gmail.com>
2023-11-29 03:51:32 +0200

    finish task6!!!

--- task6.py
+++ task6.py
@@ -3,11 +3,15 @@ from tkinter import *
 from tkinter import filedialog
 import numpy as np
 import helper_functions as hf
+from task4 import *
 from tkinter.ttk import *
 import task5_data.comparesignal2 as compare
-dropdown_var = num_entry = num_label =signal_f_entry= None
+from task6_data.TestCases.Derivative_Updated.DerivativeSignal import DerivativeSignal
+from task6_data.TestCases.Shifting_and_Folding.Shift_Fold_Signal import Shift_Fold_Signal
+
+dropdown_var = num_entry = num_label =signal_f_entry=x_values=y_values=file_name=  None
 def open_file():
-    global signal_f_entry
+    global signal_f_entry, x_values, y_values,file_name
     file_path = filedialog.askopenfilename()
     if not file_path:
         return
@@ -15,6 +19,7 @@ def open_file():
     
     signal_f_entry.delete(0, 'end')
     signal_f_entry.insert(0, file_name)
+    x_values, y_values = hf.read_signal_file(path=file_path)
   
 
 def on_select(e):
@@ -25,10 +30,10 @@ def on_select(e):
     # clear
     clear_widgets()
     if(selected_item == "Smoothing"):
-        smothing_display()
+        window_size_display()
+    elif(selected_item == "Shifted Folded Signal"):
+        k_display()
 
-    elif(selected_item == "Delaying signal" or selected_item ==  "Advancing signal"):
-        del_adv_signal_display()
     apply_button.pack()
     
 
@@ -36,46 +41,84 @@ def clear_widgets():
     num_label.forget()
     num_entry.forget()
     apply_button.forget()
-def smothing_display():
+def window_size_display():
     num_label.pack()
     num_entry.pack()
     num_label.config(text="Window size")
-def del_adv_signal_display():
+def k_display():
     num_label.pack()
     num_entry.pack()
-    num_label.config(text="K")
+    num_label.config(text="k")
     
 def apply_feature():
-    global dropdown_var
+    global dropdown_var, x_values, y_values
     selected_item = dropdown_var.get()
     if(selected_item == "Smoothing"):
-        window_size =hf.cast_to_(num_entry.get(),'int') 
-        print("call your function here")
-        print(window_size)
+        window_size =hf.cast_to_(num_entry.get(),'int')
+        x_values_smooth, y_values_smooth = Smoothing(n=x_values, x_n=y_values, window_size=window_size)
+        # hf.draw(x1=list(x_values),y1=y_values,label1="n",label2='y[n]',type='discrete',title='Smoothing Signals')
+        if(file_name == 'Signal2.txt'):
+            print('Test on Smoothing on Signal2 ...')
+            compare.SignalSamplesAreEqual(file_name='task6_data/TestCases/Moving_Average/OutMovAvgTest2.txt', samples=y_values_smooth)
+        else:
+            print('Test on Smoothing on Signal1 ...')
+            compare.SignalSamplesAreEqual(file_name='task6_data/TestCases/Moving_Average/OutMovAvgTest1.txt', samples=y_values_smooth)
     elif(selected_item == "Sharpening"):
-        print("call your function here")
-    elif(selected_item == "Delaying signal"):
-        k =hf.cast_to_(num_entry.get(),'int') 
-        print("call your function here")
-        print(k)
-        
-    elif(selected_item == "Advancing signal"):
-        k =hf.cast_to_(num_entry.get(),'int') 
-        print("call your function here")
-        print(k)
-        
+        print('Test Sharpening ...')
+        DerivativeSignal()
+
     elif(selected_item == "Folding"):
-        print("call your function here")
+        x_values_fold, y_values_fold = Folding(n=x_values, x_n=y_values)
+        print('Test on Folding ...')
+        Shift_Fold_Signal(file_name='task6_data/TestCases/Shifting_and_Folding/Output_fold.txt', Your_indices=x_values_fold, Your_samples=y_values_fold)
         
-    elif(selected_item == "Delaying folded signal"):
-        print("call your function here")
-        
-    elif(selected_item == "Advanced folded signal"):
-        print("call your function here")
-        
+    elif(selected_item == "Shifted Folded Signal"):
+        k = hf.cast_to_(num_entry.get(), 'int')
+        x_values_fold_shif, y_values_fold_shift = Folding_with_Shifting(n=x_values, x_n=y_values, k=k)
+        if(k < 0):
+            print('Test on Folding with Shifting when k = -500 ...')
+            Shift_Fold_Signal(file_name='task6_data/TestCases/Shifting_and_Folding/Output_ShiftFoldedby-500.txt', Your_indices=x_values_fold_shif, Your_samples=y_values_fold_shift)
+        else:
+            print('Test on Folding with Shifting when k = 500 ...')
+            Shift_Fold_Signal(file_name='task6_data/TestCases/Shifting_and_Folding/Output_ShifFoldedby500.txt', Your_indices=x_values_fold_shif,Your_samples=y_values_fold_shift)
     elif(selected_item == "Remove Dc"):
-        print("call your function here")
-        
+        x_values_remove_ds, y_values_remove_ds = Remove_DC(n=x_values, x_n=y_values)
+        print('Test on Remove_DC ...')
+        compare.SignalSamplesAreEqual(file_name='task5_data/RM_DC/DC_component_output.txt', samples=y_values_remove_ds)
+
+def Smoothing(n, x_n, window_size):
+    y_n = []
+    for i in range(len(x_n) - window_size + 1):
+        sum = 0
+        for j in range(window_size):
+            sum += x_n[i + j]
+        y_n.append(round(float(sum / window_size), 6))
+    return n, y_n
+
+def Shifting(n, x_n, k):
+    new_x = [a + k for a in n]
+    return new_x, x_n
+def Folding(n, x_n):
+    output_signals = {}
+    signal_map = {}
+    for i in range(len(n)):
+        signal_map[n[i]] = x_n[i]
+    for i, j in signal_map.items():
+        output_signals[int(i)] = int(signal_map[-int(i)])
+    return list(output_signals.keys()), list(output_signals.values())
+
+def Folding_with_Shifting (n, x_n, k):
+    x_folded, y_folded = Folding(n, x_n)
+    output_signals_x, output_signals_y = Shifting(x_folded,y_folded,k)
+    return output_signals_x, output_signals_y
+def Remove_DC(n, x_n):
+    amplitude, phase = dft(x_n, len(n))
+    amplitude[0] = np.abs(complex(0, 0))
+    phase[0] = np.angle(complex(0, 0))
+    final_output = idft(amplitude, phase, len(n))
+    final_output = [np.round(z, 3) for z in final_output]
+    final_output = [value.real for value in final_output]
+    return n, final_output
         
         
         
@@ -84,7 +127,7 @@ def open_gui(root):
     global window, dropdown_var,num_label ,num_entry,apply_button,signal_f_entry
     
     window = Toplevel(root)
-    window.title("Task 5")
+    window.title("Task 6")
     window.geometry("800x500")
     icon = PhotoImage(
         file='signal.png')
@@ -106,8 +149,8 @@ def open_gui(root):
                         justify='center',
                         font=('Arial', 10, 'bold'))
     # list
-    dropdown['values'] = ("Smoothing", "Sharpening", "Delaying signal", "Advancing signal",
-               "Folding", "Delaying folded signal","Advanced folded signal", "Remove Dc")
+    dropdown['values'] = ("Smoothing", "Sharpening",
+               "Folding", "Shifted Folded Signal", "Remove Dc")
     
   # conf
     dropdown.configure(height=5, width=30)
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from helper_functions import *
+from task4 import *
+import numpy as np
 from task6_data.TestCases.Derivative_Updated.DerivativeSignal import *
 
 # # this is first phase
@@ -8,11 +10,11 @@ from task6_data.TestCases.Derivative_Updated.DerivativeSignal import *
 #         temp_sum = 0
 #         for j in range(k):
 #             temp_sum += x[i + j]
-#         out_from_smooth.append(format(temp_sum / k, '.6f'))
+#         out_from_smooth.append(round(float(temp_sum / k), 6))
 #     return out_from_smooth
 # x, y = read_signal_file('task2_data/input/Signal1.txt')
 # x2 , y2 = read_signal_file('task2_data/input/Signal2.txt')
-#
+# print(x)
 # x_x = Smoothing(y, 3)
 # print(x_x)
 # print(len(x_x))
@@ -23,6 +25,12 @@ from task6_data.TestCases.Derivative_Updated.DerivativeSignal import *
 # # this the second phase
 # DerivativeSignal()
 
+# # this is third phase
+# def Delaying_or_advancing(x,y,k):
+#     new_x = [a + k for a in x]
+#     return new_x, y
+#
+#
 # # this is fourth phase
 # def Folding_a_signal(x, y):
 #     output_signals = {}
@@ -37,5 +45,31 @@ from task6_data.TestCases.Derivative_Updated.DerivativeSignal import *
 # reX, reY = Folding_a_signal(x,y)
 # print(reX)
 # print(reY)
+#
+#
+# # this is the fifth phase
+#
+# def Folding_with_Delaying_or_advancing (x,y,k):
+#     x_folded,y_folded=Folding_a_signal(x,y)
+#     output_signals_x,output_signals_y = Delaying_or_advancing(x_folded,y_folded,k)
+#     return output_signals_x, output_signals_y
+#
+# x,y = Folding_with_Delaying_or_advancing(x,y,-500)
+# print(x)
+# print(y)
 
-
+# # this is the sixth phase (final phase)
+# def Remove_DC(x, y):
+#     amplitude, phase = dft(y, len(x))
+#     amplitude[0] = np.abs(complex(0, 0))
+#     phase[0] = np.angle(complex(0, 0))
+#     final_output = idft(amplitude, phase, len(x))
+#     final_output = [np.round(z, 3) for z in final_output]
+#     final_output = [value.real for value in final_output]
+#     return x, final_output
+#
+# xx, yy = read_signal_file('task5_data/RM_DC/DC_component_input.txt')
+#
+# x, fin_out = Remove_DC(xx,yy)
+# print(x)
+# print(fin_out)
